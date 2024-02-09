52400f408c71acb9e854d5ac399f02b47c7f07d2
totorocupcake <cookcookcookie@hotmail.com>
2024-02-09 14:48:10 -0500

    function signatures for text file processing

---
+++ text_file_processor.py
@@ -0,0 +1,20 @@
+def lookup_desc (long_short , type, name, state):
+    # Given arguments find, return the matching description from in-game text files
+    # Returns None if not match
+    # long_short determines whether to return long_desc vs short_desc
+    # type determines if lookup is tile , character, object
+    
+
+def lookup_interaction (type, name, state, interaction_key):
+    # Given arguments return the matching interaction object as an array for command processing
+    # Return None if not match
+    
+
+def lookup_movable (tile_name,state):
+    # Given a tile name and state, return the matching movable flag.
+    # Return None if not match
+    
+
+def lookup_gold_amt (obj_name, state):
+    # Given an object name and state, return the matching gold_amt.
+    # Return None if not match
