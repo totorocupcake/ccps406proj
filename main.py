0aadd0c0561e4d921208fa26ee3aa1256daa2cd5
totorocupcake <cookcookcookie@hotmail.com>
2024-02-16 14:05:27 -0500

    made some template structure for game loop and initial game prompt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import text_file_processor
 import game_initialization
+import game_loop
 
 if __name__ == "__main__":
     
@@ -16,6 +17,8 @@ if __name__ == "__main__":
     Game initialization
     """
     world_state = game_initialization.intialize(object_status, character_status,world_map_status,tile_id_mapping)
+    world_state = game_initialization.initial_game_prompt(world_state)
+    
     # This should return one object which is an instance of the world_state class.
     # Within this world_state class, it contains all the tiles/characters and objects within their inventory
     # which we will manipulate and lookup within game loop.
