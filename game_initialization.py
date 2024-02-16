24e12df9571f04c288f6f2de30511954a66d7edb
totorocupcake <cookcookcookie@hotmail.com>
2024-02-16 13:57:24 -0500

    update game_initialization.py for some more template structure for the team

--- game_initialization.py
+++ game_initialization.py
@@ -1,6 +1,23 @@
+class entity:
+    # template for superclass of all other classes
+    
+class turn_count:
+    # template for superclass of tile and characters
+    # turn counting specific capabilities
 
+class tile:  
+    # template for tile
+    
+class object:
+    # template for objects (items in the game)
+class character:
+    # template for characters (includes npcs, monsters, active player)
+    
+class world_state:
+    # template of world_state
 
 def initialize(object_status, character_status,world_map_status,tile_id_mapping):
-    # Needs to return world state object instance
+    # Needs to return 1 world state object instance
+    # Instantiate all neccessary classes so we have a complete world state object to return for game loop
     
     pass
