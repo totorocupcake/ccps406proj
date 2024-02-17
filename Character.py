02347ea69e0b22092fa5c7de7136d6ded4a185e5
john-stp01 <156490725+john-stp01@users.noreply.github.com>
2024-02-17 11:24:36 -0500

    added class files and starting intialization code.  Fixed text_file_processor bug

---
+++ Character.py
@@ -0,0 +1,92 @@
+import Turn_Based_Entity
+
+class Character(Turn_Based_Entity.Turn_Based_Entity):
+
+    # CLASS CONSTRUCTOR
+    def __init__(self): 
+        self.inventory = []
+        self.__general_type = "Character"
+
+        self.__current_hp = 0
+        self.__max_hp = 0
+        self.__current_gold = 0
+
+        # should __visited by a set (of tuples) rather than a list?
+        # was: self.__visited = []
+        self.__visited = set()
+        self.__active_player = ""
+        self.__turn_counter = 0
+        self.__turn_state = ""
+
+
+    # GETTER METHODS
+    def get_inventory(self):
+        # returns a list of 'object' objects
+        return self.inventory
+
+
+    # char class specific methods:
+    def get_current_hp (self): 
+        return self.__current_hp
+    
+    def get_max_hp (self): 
+        return self.__max_hp
+    
+    def get_current_gold (self): 
+        return self.__current_gold
+    
+    def get_visited (self): 
+        return self.__visited
+    
+    def get_active_player (self): 
+        return self.__active_player
+    
+    def get_turn_counter (self): 
+        return self.__turn_counter
+    
+    def get_turn_state (self): 
+        return self.__turn_state        
+
+
+
+    # SETTER METHODS
+
+    # char class specific methods:
+    def set_current_hp (self, new_current_hp): 
+        self.__current_hp = new_current_hp
+    
+    def set_max_hp (self, new_max_hp): 
+        self.__max_hp = new_max_hp
+    
+    def set_current_gold (self, new_current_gold): 
+        self.__current_gold = new_current_gold
+    
+
+
+
+    def set_visited (self, new_visited): 
+        self.__visited = new_visited
+
+    def update_visited(self, type, name, state):
+        # check if its already in the set, 
+        #   if not, add the tuple to the set:
+        if (type, name, state) not in self.__visited:
+            self.__visited.add( (type, name, state) )
+
+
+    def set_active_player (self, is_active): 
+        if is_active == True: 
+            self.__active_player = 't'
+        else: 
+            self.__active_player = 'f'
+
+    def set_turn_counter (self): 
+        self.__turn_counter = self.__turn_counter + 1
+    
+    def set_turn_state (self, new_turn_state): 
+        self.__turn_state = new_turn_state
+
+
+
+if __name__ == "__main__": 
+    pass
---
+++ World_State.py
@@ -0,0 +1,129 @@
+import charClass
+
+# constants: (make sure they match values in load_status_data.py)
+
+WORLD_MAP_NUM_ROWS = 22
+WORLD_MAP_NUM_COLUMNS = 14
+
+
+class World_State:
+
+  # constructor:
+
+  def __init__(self):
+
+    # Private properties (with default starting values)
+    self.__turn_number = 1  
+    self.__game_won = "N"
+    self.__rent_amount = 20
+    self.__rent_due_date = 25
+    self.__characters = []  # array/list of character objects
+
+    # self.__tiles = (create and populate a 2D array/list of tile objects)
+    # for testing, populate with None:
+    self.__tiles = [[None] * WORLD_MAP_NUM_COLUMNS for _ in range(WORLD_MAP_NUM_ROWS)]
+
+
+  # methods:
+
+  def increment_turn(self, amount=1):
+    self.__turn_number += amount    
+
+
+  def set_game_won(self, flag):
+    self.__game_won = flag
+
+
+  def update_rent_turn_due(self, increment):
+    self.__rent_due_date += increment
+
+  
+  def update_rent_amount(self, increment):
+    self.__rent_amount += increment
+
+
+  def spawn_character(self, new_character):
+    self.__characters.append(new_character)
+
+    # debug:
+    print("DEBUG: spawn_char() len(characters) = ", len(self.__characters))
+
+
+  def remove_character(self, character):
+    # check to make sure inputted character is in the __characters list
+    if character in self.__characters:
+      self.__characters.remove(character)
+      # debug:
+      print("DEBUG: rem_char() len(characters) = ", len(self.__characters))
+
+
+
+  def update_tile(self, coords, new_tile):
+    pass
+
+
+  def get_desc(self, coords):
+    x_coord, y_coord = coords
+
+    # or should it be self.__tiles[y_coord][x_coord] ????
+    return self.__tiles[x_coord][y_coord]
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+  # test some methods:
+  ws = World_State()
+
+  ws.increment_turn()
+
+  ws.set_game_won("Y")  
+
+  ws.update_rent_turn_due(10)
+
+  desc = ws.get_desc((0, 0))
+
+  print()
+  if desc is not None:
+    print("desc = ", desc)
+  else:
+    print("desc = None")
+  print()
+
+  #----------------------------
+  charac1 = charClass.Character()
+
+  charac1.set_name("first character")
+
+  print("charac name = ", charac1.get_name())
+  print()
+
+  # test spawn_character() method
+  ws.spawn_character(charac1)
+  
+  charac2 = charClass.Character()
+
+  charac2.set_name("second character")
+
+  print("charac name = ", charac2.get_name())
+  print()
+
+  # test spawn_character() method
+  ws.spawn_character(charac2)
+  
+  # test remove_character() method
+  ws.remove_character(charac1)
+
+
