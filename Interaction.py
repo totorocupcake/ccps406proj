b9f2b6bcadc1068c4f2a450162326a436ee90306
john-stp01 <156490725+john-stp01@users.noreply.github.com>
2024-02-22 11:28:52 -0500

    updates to interaction processing.

--- Interaction.py
+++ Interaction.py
@@ -1,39 +1,42 @@
-class Interaction:
-  # class constructor:
-  def __init__(self):
+# don't need this now:
 
-    # properties:
-    self._entity_name = ""
-    self._entity_general_type = ""
-    self._entity_state = ""
-    # store JSON interaction data in '_interaction_data' property:
-    self._interaction_data = []
+
+# class Interaction:
+#   # class constructor:
+#   def __init__(self):
+
+#     # properties:
+#     self._entity_name = ""
+#     self._entity_general_type = ""
+#     self._entity_state = ""
+#     # store JSON interaction data in '_interaction_data' property:
+#     self._interaction_data = []
  
- # getter methods:
-  # --------------
-  def get_entity_name(self):
-    return self._entity_name
+#  # getter methods:
+#   # --------------
+#   def get_entity_name(self):
+#     return self._entity_name
 
-  def get_entity_general_type(self):
-    return self._entity_general_type
+#   def get_entity_general_type(self):
+#     return self._entity_general_type
 
-  def get_entity_state(self):
-    return self._entity_state
+#   def get_entity_state(self):
+#     return self._entity_state
   
-  def get_interaction_data(self):
-    return self._interaction_data 
+#   def get_interaction_data(self):
+#     return self._interaction_data 
   
 
-  # setter methods:
-  # --------------
-  def set_entity_name(self, new_name):
-    self._entity_name = new_name  
+#   # setter methods:
+#   # --------------
+#   def set_entity_name(self, new_name):
+#     self._entity_name = new_name  
 
-  def set_entity_general_type(self, new_general_type):
-    self._entity_general_type = new_general_type
+#   def set_entity_general_type(self, new_general_type):
+#     self._entity_general_type = new_general_type
 
-  def set_entity_state(self, new_state):
-    self._entity_state = new_state
+#   def set_entity_state(self, new_state):
+#     self._entity_state = new_state
 
-  def set_interaction_data(self, new_interaction_data):
-    self._interaction_data = new_interaction_data
+#   def set_interaction_data(self, new_interaction_data):
+#     self._interaction_data = new_interaction_data
