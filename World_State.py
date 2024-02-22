4cc551532c90af5cfecd0dc2f34b50cbc4c27bf5
totorocupcake <cookcookcookie@hotmail.com>
2024-02-22 12:23:24 -0500

    initial cow, chicken grazing mechanic, not yet linked to game

--- World_State.py
+++ World_State.py
@@ -260,20 +260,44 @@ class World_State:
       pass
     
     elif charac.name == "chicken" and charac.state =="wild":
-      # write behavior of a wild chicken
-      pass
+      # fast mover, it moves once every turn
+      return graze()
     
     elif charac.name == "cow" and charac.state =="wild":
-      # write behavior of a wild cow
-      pass
+      # cow is slower moving compared to chicken, it only moves once every 2 turns
+      if (self.get_turn_number() % 2) == 0:
+        return graze()
+      else:
+        pass
     
     else:
       # default do nothing if no behavior defined for character
       pass
 
-      def graze():
-        pass
+    def graze():
+      # get_next_action sub function to define a generic "Grazing" behavior for wild cow/chicken
+      # should move randomly n,w,e,w if there is grassland available for them
         
+      random.seed(42) # select seed for reproducible results for testing
+      available_directions = [] 
+      current_x,current_y = charac.get_coords()
+        
+      max_cols = len(self.get_tiles()[0])-1
+      max_rows = len(self.get_tiles())-1
+        
+      # check if n,s,e,w is available to move to and is a grassland
+      if current_y+1 >= 0 and current_y+1 <= max_cols and self.get_tiles()[current_x][current_y+1].name == "grasslands":
+        available_directions.append("n")
+      if current_y-1 >= 0 and current_y-1 <= max_cols and self.get_tiles()[current_x][current_y-1].name == "grasslands":
+        available_directions.append("s")
+      if current_x+1 >= 0 and current_x+1 <= max_rows and self.get_tiles()[current_x+1][current_y].name == "grasslands":
+        available_directions.append("e")
+      if current_x-1 >= 0 and current_x-1 <= max_rows and self.get_tiles()[current_x-1][current_y].name == "grasslands":
+        available_directions.append("w")
+        
+      # returns random direction from available_directions list
+      return random.choice(available_directions)
+    
         
 
 
