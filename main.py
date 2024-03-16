61ce829e75ee7ebf46de998ba0bf748fcd7cf977
totorocupcake <cookcookcookie@hotmail.com>
2024-03-16 16:44:40 -0400

    more self-explanatory comments removed

--- main.py
+++ main.py
@@ -7,25 +7,17 @@ STARTING_RENT_DUE_DATE = 100
 
 if __name__ == "__main__":
     
-    """
-    Text parsing
-    """
     # Initialize read-only files into data object which is a Singleton class.
     data = Data.Data()
-    
-    """
-    Game initialization
-    """
+
     world_state = game_initialization.initialize(STARTING_RENT_AMOUNT, STARTING_RENT_DUE_DATE)
-    
-    """
-    Game Loop
-    """
+
     world_state=game_loop.play_game(world_state)
     
     """
     Exit game loop
     """
+    
     if (world_state.get_game_won() == 'Y'): 
         print("You win!")
     else: 
